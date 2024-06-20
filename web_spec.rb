b71d686150821ef9967a1e77cc582255c245a4f0
Bruno FABRE <bruno.fabre@protonmail.com>
2024-06-20 18:43:58 +0000

    Update 16 files
    - /README.md
    - /_res/kitchen.gif
    - /web_spec.rb
    - /Gemfile
    - /roles/nginx/chefignore
    - /roles/nginx/kitchen.yml
    - /roles/nginx/defaults/main.yml
    - /roles/nginx/handlers/main.yml
    - /roles/nginx/meta/main.yml
    - /roles/nginx/tasks/main.yml
    - /roles/nginx/test/integration/default/default.yml
    - /roles/nginx/test/integration/default/web_spec.rb
    - /roles/nginx/tests/inventory
    - /roles/nginx/tests/test.yml
    - /roles/nginx/vars/main.yml
    - /install_nginx.yml

---
+++ web_spec.rb
@@ -0,0 +1,25 @@
+# use basic tests
+describe package("nginx") do
+    it { should be_installed }
+  end
+  
+  describe port(80) do
+    it { should be_listening }
+    its('protocols') {should include 'tcp'}
+  end
+  
+  describe port(443) do
+    it { should_not be_listening }
+  end
+  
+  describe host('localhost') do
+    its('ipaddress') { should include '127.0.0.1' }
+  end
+  
+  # implement os dependent tests
+  web_user = "www-data"
+  web_user = "nginx" if os[:family] == "centos"
+  
+  describe user(web_user) do
+    it { should exist }
+  end
